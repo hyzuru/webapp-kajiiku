8e0305e94a01bafca484d3d18463efec02c3702e
Jackie Yeung <jackie@Jackies-MacBook-Pro.local>
2019-08-15 12:22:25 +0900

    added category pages

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
+// Open up the side menu
+const openMenu = () => {
+  document.querySelector("#menu").open();
+};
+
+// Dynamically Change the Title Bar
+document.addEventListener("prechange", ({ target, tabItem }) => {
+  if (target.matches("#tabbar")) {
+    document.querySelector(
+      "#home-toolbar .center"
+    ).innerHTML = tabItem.getAttribute("label");
+  }
+});
+
 document.addEventListener("init", function(event) {
   var myNavigator = document.getElementById("myNavigator");
   myNavigator.pushPage("page1.html");
