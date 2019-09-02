22af5ae4fbe170dc4a67d2a49e4352ecbf380a4c
Jackie Yeung <jackie@Jackies-MacBook-Pro.local>
2019-09-02 11:25:24 +0900

    データをフィルター機能追加

--- public/js/todo.js
+++ public/js/todo.js
@@ -104,7 +104,47 @@
     if (event.target.classList.contains('js-delete-todo')) {
       const itemKey = event.target.parentElement.parentElement.dataset.key;
       console.log(event.target.parentElement.dataset);
-      deleteTodo(itemKey);
+      // deleteTodo(itemKey);
     }
   
   });
+
+  function showModal(el) {
+            
+    var elname = el.tagName ;
+    // get text of the item that was clicked
+    console.log(elname);
+    var inputTag = el.parentNode.firstChild;
+    // console.log(inputTag.tagName);
+    var dataKey = inputTag.getAttribute('id');
+    console.log(inputTag.getAttribute('id'));
+
+    var todoTextLabel = el.parentElement.querySelector('span.todo-itemName').textContent;
+    console.log(todoTextLabel);
+
+    var todoTextComment = el.parentElement.querySelector('span.todo-itemComment').textContent;
+      var todoTextComment = todoTextComment.replace(/\s+/g, '');
+      if (todoTextComment != "") {
+        todoTextComment = todoTextComment.replace(/\s+/g, '');
+      } else {
+        // console.log(todoTextComment);
+        todoTextComment = "コメントなし"
+      }
+      console.log(todoTextComment);
+    // display the modal
+
+  var modal = document.querySelector('ons-modal.taskDetailsModal');
+  modal.show();
+         
+  var mText = modal.querySelector('p') ;
+
+  mText.innerHTML =  todoTextLabel;
+  mText.innerHTML =  "<div class='noDisplay'>" + dataKey+"</div>"+todoTextLabel;
+  mText.innerHTML += "<br><div class='todo-hLine'></div><br>"+todoTextComment;
+
+  var closeButton = document.querySelector('.closeBtn');
+  closeButton.onclick = function(){
+    // var modal = document.querySelector('ons-modal');
+    modal.hide();
+  };            
+}
