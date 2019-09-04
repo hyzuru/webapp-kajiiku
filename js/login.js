2e6b68a5fa9c7c3b70959c8ba6843bcf8c2fc6c2
hyzuru <asatenshi@gmail.com>
2019-09-05 00:31:19 +0900

    update categories send to db and show as images

--- js/login.js
+++ js/login.js
@@ -1,29 +1,30 @@
 // Login Page
 const login = () => {
-  const myEmail = document.querySelector("#email").value;
-  const password = document.querySelector("#password").value;
-  console.log(myEmail);
-  console.log(password);
-  firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
-  .then(function() {
-  firebase.auth().signInWithEmailAndPassword(myEmail, password)
-  .then(function() {
-    console.log('サインアップしました。');
-    const navigator = document.querySelector("#navigator");
-    navigator.resetToPage("home.html");
-  })
-  .catch(function(error) {
-    console.log('サインアップできませんでした。');
-  });
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-});
+  // const myEmail = document.querySelector("#email").value;
+  // const password = document.querySelector("#password").value;
+  // console.log(myEmail);
+  // console.log(password);
+  // firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+  // .then(function() {
+  // firebase.auth().signInWithEmailAndPassword(myEmail, password)
+  // .then(function() {
+  //   console.log('サインアップしました。');
+  const navigator = document.querySelector("#navigator");
+  navigator.resetToPage("home.html");
+  // })
   //   .catch(function(error) {
-// // 登録に失敗した時の処理
-//     alert('登録できません（' + error.message + '）');
-// });
+  //     console.log('サインアップできませんでした。');
+  //   });
+  // }).catch(function(error) {
+  //   // Handle Errors here.
+  //   var errorCode = error.code;
+  //   var errorMessage = error.message;
+  // });
+
+  //   .catch(function(error) {
+  // // 登録に失敗した時の処理
+  //     alert('登録できません（' + error.message + '）');
+  // });
   // if (myEmail === "" && password === "") {
   //   const navigator = document.querySelector("#navigator");
   //   navigator.resetToPage("home.html");
@@ -36,41 +37,48 @@ const signUp = () => {
   const password = document.querySelector("#newPassword").value;
   console.log(myEmail);
   console.log(password);
-  var elements = document.getElementsByName( "answer" ) ;
+  var elements = document.getElementsByName("answer");
   // 選択状態の値を取得
-  for ( var a="", i=elements.length; i--; ) {
-    if ( elements[i].checked ) {
-      var a = elements[i].value ;
-      break ;
+  for (var a = "", i = elements.length; i--; ) {
+    if (elements[i].checked) {
+      var a = elements[i].value;
+      break;
     }
   }
-console.log(a);
-firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE)
-.then(function() {
-  firebase.auth().createUserWithEmailAndPassword(myEmail, password)
-  .then(function() {
-    console.log('サインアップしました。');
-    const navigator = document.querySelector("#navigator");
-    navigator.resetToPage("home.html");
-    var user = firebase.auth().currentUser;
-    user.providerData.forEach(function (profile) {
-      const UID = user.uid;
-      firebase.database().ref('usertable').push({
-        email:myEmail,
-        password:password,
-        role:a,
-        userUID:UID,
-        partnerID:""
-      })
+  console.log(a);
+  firebase
+    .auth()
+    .setPersistence(firebase.auth.Auth.Persistence.NONE)
+    .then(function() {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(myEmail, password)
+        .then(function() {
+          console.log("サインアップしました。");
+          const navigator = document.querySelector("#navigator");
+          navigator.resetToPage("home.html");
+          var user = firebase.auth().currentUser;
+          user.providerData.forEach(function(profile) {
+            const UID = user.uid;
+            firebase
+              .database()
+              .ref("usertable")
+              .push({
+                email: myEmail,
+                password: password,
+                role: a,
+                userUID: UID,
+                partnerID: ""
+              });
+          });
+        })
+        .catch(function(error) {
+          console.log("サインアップできませんでした。");
+        });
+    })
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
     });
-  })
-  .catch(function(error) {
-    console.log('サインアップできませんでした。');
-  });
-})
-.catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-});
 };
