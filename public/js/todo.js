a0a4a33a0e46e6e96bb473daa3d5f26eacd8b333
Jackie Yeung <jackie@Jackies-MacBook-Pro.local>
2019-09-06 03:00:14 +0900

    submit scores to tasks and update profile score

--- public/js/todo.js
+++ public/js/todo.js
@@ -2,7 +2,7 @@
   
   let todoItems = [];
 
-  function addTodo(text,comment) {
+  function addTodo(text, comment, category) {
     const todo = {
       text,
       comment,
@@ -20,6 +20,29 @@
       })
     loadPage('home.html'); 
   }
+  // function toggleDone(key) {
+  //   const index = todoItems.findIndex(item => item.id === Number(key));
+  //   todoItems[index].checked = !todoItems[index].checked;
+  
+  //   const item = document.querySelector(`[data-key='${key}']`);
+  //   if (todoItems[index].checked) {
+  //     item.classList.add("done");
+  //     // document.querySelector
+  //   } else {
+  //     item.classList.remove("done");
+  //   }
+  
+  // }
+  
+  // function deleteTodo(key) {
+  //   todoItems = todoItems.filter(item => item.id !== Number(key));
+  //   const item = document.querySelector(`[data-key='${key}']`);
+  //   item.remove();
+  
+  //   const list = document.querySelector(".js-todo-list");
+  //   if (todoItems.length === 0) list.innerHTML = "";
+  // }
+  
 
   
   const form = document.querySelector('.js-form');
@@ -36,13 +59,25 @@
     const text = selector.textContent.trim();
     console.log(text);
 
-    
-    if (text !== 'タスクを選ぶ') {
-      addTodo(text,comment);
-      selector.textContent = 'タスクを選ぶ';
-      input.value = '';
-    } 
-    else {
+    const getCategory = document.querySelector(".js-todo-selector div div")
+    .outerHTML;
+  console.log(getCategory);
+  console.log(
+    getCategory
+      .split('="')
+      .pop()
+      .split('"')[0]
+  );
+  const category = getCategory
+    .split('="')
+    .pop()
+    .split('"')[0];    
+
+    if (text !== "タスクを選ぶ") {
+      addTodo(text, comment, category);
+      selector.textContent = "タスクを選ぶ";
+      input.value = "";
+    } else {
       var msg = "タスクは選択されていません。";
       var d1 = document.querySelector(".js-form div.warningMsg");
       d1.innerHTML = msg;
@@ -67,42 +102,42 @@
   
   // });
 
-  function showModal(el) {
+//   function showModal(el) {
             
-    var elname = el.tagName ;
-    // get text of the item that was clicked
-    console.log(elname);
-    var inputTag = el.parentNode.firstChild;
-    // console.log(inputTag.tagName);
-    var dataKey = inputTag.getAttribute('id');
-    console.log(inputTag.getAttribute('id'));
+//     var elname = el.tagName ;
+//     // get text of the item that was clicked
+//     console.log(elname);
+//     var inputTag = el.parentNode.firstChild;
+//     // console.log(inputTag.tagName);
+//     var dataKey = inputTag.getAttribute('id');
+//     console.log(inputTag.getAttribute('id'));
 
-    var todoTextLabel = el.parentElement.querySelector('span.todo-itemName').textContent;
-    console.log(todoTextLabel);
+//     var todoTextLabel = el.parentElement.querySelector('span.todo-itemName').textContent;
+//     console.log(todoTextLabel);
 
-    var todoTextComment = el.parentElement.querySelector('span.todo-itemComment').textContent;
-      var todoTextComment = todoTextComment.replace(/\s+/g, '');
-      if (todoTextComment != "") {
-        todoTextComment = todoTextComment.replace(/\s+/g, '');
-      } else {
-        // console.log(todoTextComment);
-        todoTextComment = "コメントなし"
-      }
-      console.log(todoTextComment);
-    // display the modal
+//     var todoTextComment = el.parentElement.querySelector('span.todo-itemComment').textContent;
+//       var todoTextComment = todoTextComment.replace(/\s+/g, '');
+//       if (todoTextComment != "") {
+//         todoTextComment = todoTextComment.replace(/\s+/g, '');
+//       } else {
+//         // console.log(todoTextComment);
+//         todoTextComment = "コメントなし"
+//       }
+//       console.log(todoTextComment);
+//     // display the modal
 
-  var modal = document.querySelector('ons-modal.taskDetailsModal');
-  modal.show();
+//   var modal = document.querySelector('ons-modal.taskDetailsModal');
+//   modal.show();
          
-  var mText = modal.querySelector('p') ;
+//   var mText = modal.querySelector('p') ;
 
-  mText.innerHTML =  todoTextLabel;
-  mText.innerHTML =  "<div class='noDisplay'>" + dataKey+"</div>"+todoTextLabel;
-  mText.innerHTML += "<br><div class='todo-hLine'></div><br>"+todoTextComment;
+//   mText.innerHTML =  todoTextLabel;
+//   mText.innerHTML =  "<div class='noDisplay'>" + dataKey+"</div>"+todoTextLabel;
+//   mText.innerHTML += "<br><div class='todo-hLine'></div><br>"+todoTextComment;
 
-  var closeButton = document.querySelector('.closeBtn');
-  closeButton.onclick = function(){
-    // var modal = document.querySelector('ons-modal');
-    modal.hide();
-  };            
-}
+//   var closeButton = document.querySelector('.closeBtn');
+//   closeButton.onclick = function(){
+//     // var modal = document.querySelector('ons-modal');
+//     modal.hide();
+//   };            
+// }
