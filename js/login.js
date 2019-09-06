03c60be455fe7e07dd1ed70b530b6cebb1d1daa3
wakaolian <last62us@gmail.com>
2019-09-06 21:15:19 +0900

    ログアウト機能追加

--- js/login.js
+++ js/login.js
@@ -1,30 +1,31 @@
 // Login Page
+
+
 const login = () => {
   const myEmail = document.querySelector("#email").value;
   const password = document.querySelector("#password").value;
   console.log(myEmail);
   console.log(password);
-
+  firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+  .then(function() {
   firebase.auth().signInWithEmailAndPassword(myEmail, password)
   .then(function() {
-    console.log('サインアップしました。');
+    console.log('ログインしました。');
     const navigator = document.querySelector("#navigator");
     navigator.resetToPage("home.html");
+
   })
   .catch(function(error) {
-    console.log('サインアップできませんでした。');
+    console.log('ログインできませんでした。');
+        // 登録に失敗した時の処理
+
+        ons.notification.alert('ログインできません（' + error.message + '）');
   });
-  //   .catch(function(error) {
-// // 登録に失敗した時の処理
-//     alert('登録できません（' + error.message + '）');
-// });
-  // if (myEmail === "" && password === "") {
-  //   const navigator = document.querySelector("#navigator");
-  //   navigator.resetToPage("home.html");
-  // } else {
-  //   ons.notification.alert("Wrong username/password combination");
-  // }
+
+})
+
 };
+
 const signUp = () => {
   const myEmail = document.querySelector("#newEmail").value;
   const password = document.querySelector("#newPassword").value;
@@ -40,6 +41,8 @@ const signUp = () => {
   }
 console.log(a);
 
+firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+.then(function() {
   firebase.auth().createUserWithEmailAndPassword(myEmail, password)
   .then(function() {
     console.log('サインアップしました。');
@@ -53,11 +56,38 @@ console.log(a);
         password:password,
         role:a,
         userUID:UID,
-        partnerID:""
+        partnerID:"",
+        point:0
+
       })
+
     });
   })
   .catch(function(error) {
     console.log('サインアップできませんでした。');
+        // 登録に失敗した時の処理
+
+        ons.notification.alert('登録できません（' + error.message + '）');
   });
+})
+
 };
+
+
+const doLogout = () => {
+    firebase.auth().signOut()
+    .then(function() {
+        console.log('ログアウトしました。');
+        const navigator = document.querySelector("#navigator");
+        navigator.resetToPage("login.html");
+
+    
+
+      })
+      .catch(function(error) {
+        console.log('ログアウトできませんでした。');
+            // 登録に失敗した時の処理
+    
+            ons.notification.alert('logout is muri' + error.message + '）');
+      });
+  };
